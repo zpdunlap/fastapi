22f98c135d3c38824c3b170664be7bfce36a2063
Zach <fireknight95@gmail.com>
2023-01-02 13:54:53 -0500

    import middleware

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 # import mysql.connector
 import pymysql
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
 middleware = [
