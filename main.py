3c8fa5d8eeddd17795b61880505bdff58f2342c0
Zach <fireknight95@gmail.com>
2023-01-01 22:08:10 -0500

    everythin

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
