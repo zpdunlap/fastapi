94cddf2f8f8fb1e1bbfb3279b8159e4448f343ff
Zach <fireknight95@gmail.com>
2023-01-01 22:13:46 -0500

    route

--- main.py
+++ main.py
@@ -73,6 +73,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/items/{id}", response_class=HTMLResponse)
+@app.get("/items", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("index.html", {"request": request, "id": id})
