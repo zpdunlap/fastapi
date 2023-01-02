e5bc85b6f9f9661df23c664478dda98ea2d754b3
Zach <fireknight95@gmail.com>
2023-01-01 21:57:06 -0500

    test

--- main.py
+++ main.py
@@ -1,57 +1,78 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-# import mysql.connector
-import pymysql
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# # import mysql.connector
+# import pymysql
+
+# app = FastAPI()
+
+
+
+# class Msg(BaseModel):
+#     msg: str
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World. Welcome to FastAPI!"}
+
+# @app.get("/data")
+# async def html():
+#     return 
+
+# @app.get("/data")
+# async def get_marked_systems():
+    
+#     connection = pymysql.connect( host='containers-us-west-32.railway.app', user='root', passwd='Jyfcd452Xe3tmMsFLYDY', port=5522, db='railway' )
+#     with connection.cursor() as cursor:
+#         # Read a single record
+#         sql = "SELECT `name` FROM `marked_systems` WHERE `id`=%s"
+#         cursor.execute(sql, ('1',))
+#         result = cursor.fetchone()
+#         print(result)
+#     # doQuery( myConnection )
+#     # myConnection.close()
+#     # cnx = mysql.connector.connect(user='root', password='Jyfcd452Xe3tmMsFLYDY', host='containers-us-west-32.railway.app:5522', database='railway')
+#     # cursor = cnx.cursor()
+
+#     # query = 'SELECT * FROM marked_systems'
+#     # cursor.execute(query)
+
+#     # rows = []
+#     # for row in cursor:
+#     #     print(row)
+#     #     rows.append(row)
+
+#     return {"data": result}
+#     cursor.close()
+#     cnx.close()
+
+# @app.get("/path")
+# async def demo_get():
+#     return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+
+
+# @app.post("/path")
+# async def demo_post(inp: Msg):
+#     return {"message": inp.msg.upper()}
+
+
+# @app.get("/path/{path_id}")
+# async def demo_get_path_id(path_id: int):
+#     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-class Msg(BaseModel):
-    msg: str
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-@app.get("/data")
-async def get_marked_systems():
-    
-    connection = pymysql.connect( host='containers-us-west-32.railway.app', user='root', passwd='Jyfcd452Xe3tmMsFLYDY', port=5522, db='railway' )
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `name` FROM `marked_systems` WHERE `id`=%s"
-        cursor.execute(sql, ('1',))
-        result = cursor.fetchone()
-        print(result)
-    # doQuery( myConnection )
-    # myConnection.close()
-    # cnx = mysql.connector.connect(user='root', password='Jyfcd452Xe3tmMsFLYDY', host='containers-us-west-32.railway.app:5522', database='railway')
-    # cursor = cnx.cursor()
-
-    # query = 'SELECT * FROM marked_systems'
-    # cursor.execute(query)
-
-    # rows = []
-    # for row in cursor:
-    #     print(row)
-    #     rows.append(row)
-
-    return {"data": result}
-    cursor.close()
-    cnx.close()
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
